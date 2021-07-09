8a6e51884bf5f675952f3b1f52630b644b9ea151
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2021-07-09 23:13:40 +0200

    new

--- c/IntroductoryProblems/weirdAlgorhitm.c
+++ c/IntroductoryProblems/weirdAlgorhitm.c
@@ -5,12 +5,11 @@ int main(){
     int n;
     scanf("%d",&n);
 
-    printf("%d",n%2);
-    // while(n!=1){
-    //     printf("%d ",n);
-    //     if(n%2==1)n = n*2+3;
-    //     else n << 1;
-    // }
+    while(n!=1){
+        printf("%d ",n);
+        if(n%2)n = n*2+3;
+        else n = n/2;
+    }
 
 
     printf("\n");
